10071fb6503a3b60d3d8e5375b89e806a80bb1e4
Conner McCabe <conner.p.mccabe@gmail.com>
2020-08-28 16:18:56 -0500

    adding icons to header w/ marterial

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import LanguageIcon from '@material-ui/icons/Language';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import MenuIcon from '@material-ui/icons/Menu';
 import './Header.css'
 
 function Header() {
   return ( 
     <div className="header">
       <img src="https://i.pinimg.com/originals/7b/03/4a/7b034a90114ee20e4fa002c80d276444.png" alt="Logo"></img>
-      <h1>Header of the test page</h1>
+      <div className="headerSearch">
+        <input className=""></input>
+
+      </div>
+      <div className="headerUtility">
+        <button>Become a Host</button>
+        <LanguageIcon />
+        <div className="headerSignUp">
+          <MenuIcon />
+          <AccountCircleIcon />
+        </div>
+      </div>
     </div>
   )
 }
