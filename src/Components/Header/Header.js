c07ffab460703f558ebf8a27af5a6772442fa096
Conner McCabe <conner.p.mccabe@gmail.com>
2020-09-22 12:00:18 -0500

    adding styling

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,12 +3,13 @@ import LanguageIcon from '@material-ui/icons/Language';
 import SearchIcon from '@material-ui/icons/Search';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import MenuIcon from '@material-ui/icons/Menu';
+import LocalDiningIcon from '@material-ui/icons/LocalDining';
 import './Header.css'
 
 function Header() {
   return ( 
     <div className="header">
-      <img src="https://i.pinimg.com/originals/7b/03/4a/7b034a90114ee20e4fa002c80d276444.png" alt="Logo"></img>
+      <LocalDiningIcon />
       <div className="headerSearch">
         <input className="" placeholder="Start your Search"></input>
         <SearchIcon />
